0dd6434fa4a6b908a56400363a3d71bd736d831c
Bronco <bronco@warriordudimanche.net>
2013-06-17 10:53:03 +0200

    Version 1.4
    Ajout de filtres d'image: couleur et taille

--- googol.php
+++ googol.php
@@ -12,21 +12,21 @@
 
 	define('REGEX_VID','#(?:<img.*?src="([^"]+)".*?width="([0-9]+)".*?)?<h3 class="r">[^<]*<a href="/url\?q=(.*?)(?:&amp;|&).*?">(.*?)</a>.*?<cite[^>]*>(.*?)</cite>.*?<span class="(?:st|f)">(.*?)(?:</span></td>|</span><br></?div>)#');
 	define('REGEX_VID_THMBS','#<img.*?src="([^"]+)".*?width="([0-9]+)"#');
-	define('TPL','<div class="result"><a rel="noreferrer" href="#link"><h3 class="title">#title</h3>#link</a>#wot<p class="description">#description</p></div>');
+	define('TPL','<li class="result"><a rel="noreferrer" href="#link"><h3 class="title">#title</h3>#link</a>#wot<p class="description">#description</p></li>');
 	define('TPLIMG','<div class="image" ><p><a rel="noreferrer" href="#link" title="#link">#thumbs</a></p><p class="description">#W x #H (#SZ ko)<a class="source" href="#site" title="#site"> &#9658;</a></p></div>');
 	define('TPLVID','<div class="video" ><h3><a rel="noreferrer" href="#link" title="#link">#titre</a></h3><a class="thumb" rel="noreferrer" href="#link" title="#link">#thumbs</a><p class="site">#site</p><p class="description">#description</p></div>');
 	define('LOGO1','<a href="'.RACINE.'"><em class="g">G</em><em class="o1">o</em>');
 	define('LOGO2','<em class="o2">o</em><em class="g">g</em><em class="o1">o</em><em class="l">l</em></a>');
-	
+
 	define('SAFESEARCH_ON','&safe=on');
 	define('SAFESEARCH_IMAGESONLY','&safe=images');
 	define('SAFESEARCH_OFF','&safe=off');
 	define('SAFESEARCH_LEVEL',SAFESEARCH_OFF);// SAFESEARCH_ON, SAFESEARCH_IMAGESONLY, SAFESEARCH_OFF
 
-	define('URL','https://www.google.com/search?hl='.LANGUAGE.SAFESEARCH_LEVEL.'&q=');
+	define('URL','https://www.google.com/search?hl='.LANGUAGE.SAFESEARCH_LEVEL.'&id=hp&q=');
 	define('URLIMG','&tbm=isch&biw=1920&bih=1075');
 	define('URLVID','&tbm=vid');
-	define('VERSION','v1.3d');
+	define('VERSION','v1.4');
 	define('USE_GOOGLE_THUMBS',false);
 	define('THEME','style_google.css');
 
@@ -53,11 +53,32 @@
 		'Filter on'=>myhtmlentities('Filtre activé'),
 		'Filter off'=>myhtmlentities('Filtre désactivé'),
 		'Filter images only'=>myhtmlentities('Filtre activé sur les images'),
+		'red'=>'rouge',
+		'yellow'=>'jaune',
+		'green'=>'vert',
+		'white'=>'blanc',
+		'gray'=>'gris',
+		'teal'=>'sarcelle',
+		'black'=>'noir',
+		'pink'=>'rose',
+		'blue'=>'bleu',
+		'brown'=>'marron',
+		'Black_and_white'=>'Noir_et_blanc',
+		'Color'=>'couleur',
+		'all colors'=>'toutes les couleurs',
+		'all sizes'=>'toutes les tailles',
+		'Select a color'=>'Filtrer par couleur',
+		'Select a size'=>'Filtrer par size',
+		'Big'=>'Grande',
+		'Medium'=>'Moyenne',
+		'Icon'=>'Petite',
 		);
 
 
-
-	function aff($a,$stop=true){echo 'Arret a la ligne '.__LINE__.' du fichier '.__FILE__.'<pre>';var_dump($a);echo '</pre>';if ($stop){exit();}}
+ 	#######################################################################
+	## Fonctions
+	#######################################################################
+	function aff($a,$stop=true,$line){echo 'Arret a la ligne '.$line.' du fichier '.__FILE__.'<pre>';var_dump($a);echo '</pre>';if ($stop){exit();}}
 	function msg($m){global $lang;if(isset($lang[LANGUAGE][$m])){return $lang[LANGUAGE][$m];}else{return $m;}}
 	function lang($default='fr'){if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){$l=explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);return substr($l[0],0,2);}else{return $default;}}
 	function return_safe_search_level(){
@@ -118,9 +139,9 @@
 		}
 	}
 	function parse_query($query,$start=0){
-		global $mode;
+		global $mode,$filtre;
 		if ($mode=='web'){ 
-			$page=file_curl_contents(URL.str_replace(' ','+',urlencode($query)).'&start='.$start);
+			$page=file_curl_contents(URL.str_replace(' ','+',urlencode($query)).'&start='.$start.'&num=100');
 			if (!$page){return false;}
 			preg_match_all(REGEX_WEB, $page, $r);
 			preg_match_all(REGEX_PAGES,$page,$p);
@@ -136,7 +157,8 @@
 				);
 			return $retour;
 		}elseif ($mode=='images'){ 
-			$page=file_curl_contents(URL.str_replace(' ','+',urlencode($query)).URLIMG.'&start='.$start);			
+			if (!empty($filtre)){$f='&tbs='.$filtre;}else{$f='';}
+			$page=file_curl_contents(URL.str_replace(' ','+',urlencode($query)).URLIMG.$f.'&start='.$start);			
 			if (!$page){return false;}
 			preg_match_all(REGEX_IMG,$page,$r);
 			preg_match_all(REGEX_PAGES,$page,$p);
@@ -156,6 +178,7 @@
 				'query'=>$query,
 				'mode'=>$mode
 				);			
+		
 			return $retour;		
 		}elseif($mode=="videos"){
 			$page=file_curl_contents(URL.str_replace(' ','+',urlencode($query)).URLVID.'&start='.$start);
@@ -180,9 +203,11 @@
 	}
 
 	function render_query($array){
-		global $start,$langue,$mode;
+		global $start,$langue,$mode,$couleur,$taille;
 		if (!is_array($array)||count($array['links'])==0){echo '<div class="noresult"> '.msg('no results for').' <em>'.$array['query'].'</em> </div>';return false;}
+		
 		if ($mode=='web'){
+			echo '<ol>';
 			foreach ($array['links'] as $nb => $link){
 				$r=str_replace('#link',urldecode($link),TPL);
 				$r=str_replace('#title',$array['titles'][$nb],$r);
@@ -196,6 +221,7 @@
 
 				echo $r;
 			}
+			echo '</ol>';
 		}elseif ($mode=='images'){
 			foreach ($array['links'] as $nb => $link){
 				$r=str_replace('#link',$link,TPLIMG);
@@ -231,12 +257,12 @@
 
 		if($array['nb_pages'] != 0){
 			echo '<hr/><p class="footerlogo">'.LOGO1.str_repeat('<em class="o2">o</em>', $array['nb_pages']-1).LOGO2.'</p><div class="pagination">';
-			if ($start>0){echo '<a class="previous" title="'.msg('previous').'" href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.($start-10).'&lang='.$langue.'">&#9668;</a>';}
+			if ($start>0){echo '<a class="previous" title="'.msg('previous').'" href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.($start-10).'&lang='.$langue.'&couleur='.$couleur.'&taille='.$taille.'">&#9668;</a>';}
 			for ($i=0;$i<$array['nb_pages']-1;$i++){
 				if ($i*10==$array['current_page']){echo '<em>'.($i+1).'</em>';}
-				else{echo '<a href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.$i.'0&lang='.$langue.'">'.($i+1).'</a>';}
+				else{echo '<a href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.$i.'0&lang='.$langue.'&couleur='.$couleur.'&taille='.$taille.'">'.($i+1).'</a>';}
 			}
-			if ($start<($array['nb_pages']-2)*10){echo '<a class="next" title="'.msg('next').'" href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.($start+10).'&lang='.$langue.'">&#9658;</a>';}
+			if ($start<($array['nb_pages']-2)*10){echo '<a class="next" title="'.msg('next').'" href="?q='.urlencode($array['query']).'&mod='.$mode.'&start='.($start+10).'&lang='.$langue.'&couleur='.$couleur.'&taille='.$taille.'">&#9658;</a>';}
 			
 			echo  '</div>';
 		}
@@ -258,11 +284,18 @@
 	}
 	function clear_cache($delay=180){$fs=glob('thumbs/*'); if(!empty($fs)){foreach ($fs as $file){if (@date('U')-@date(filemtime($file))>$delay){unlink ($file);}}}}
 	function is_active($first,$second){if ($first==$second){echo 'active';}else{echo '';}}
- 	function myhtmlentities($string){	$temp=htmlentities($string, ENT_QUOTES, 'UTF-8');if ($temp==''){return $string;}else{return $temp;}}
+ 	function myhtmlentities($string){$temp=htmlentities($string, ENT_QUOTES, 'UTF-8');if ($temp==''){return $string;}else{return $temp;}}
+ 	
 
-	// Gestion GET
+ 	#######################################################################
+	## Gestion GET
+	#######################################################################
 	if (isset($_GET['mod'])){$mode=$_GET['mod'];}else{$mode='web';}
 	if (isset($_GET['start'])){$start=$_GET['start'];}else{$start='';}
+	if (!empty($_GET['couleur'])&&empty($_GET['taille'])){$filtre=$couleur=$_GET['couleur'];$taille='';}
+	elseif (!empty($_GET['taille'])&&empty($_GET['couleur'])){$filtre=$taille=$_GET['taille'];$couleur='';}
+	elseif (!empty($_GET['taille'])&&!empty($_GET['couleur'])){$taille=$_GET['taille'];$couleur=$_GET['couleur'];$filtre=$couleur.','.$taille;}
+	else{$filtre=$taille=$couleur='';}
 	if (isset($_GET['q'])){
 		$q_raw=$_GET['q'];
 		if (!$q_txt=myhtmlentities($_GET['q'])){$q_txt=$_GET['q'];} 
@@ -282,6 +315,7 @@
 	<?php if (is_file('favicon.png')){echo '<link rel="shortcut icon" href="favicon.png" /> ';}?>
 	<link rel="stylesheet" type="text/css" href="<?php echo THEME;?>"  media="screen" />
 	<link rel="search" type="application/opensearchdescription+xml" title="<?php echo msg('Googol - google without lies'); ?>" href="<?php echo RACINE;?>/googol.xml">
+
 	<!--[if IE]><script> document.createElement("article");document.createElement("aside");document.createElement("section");document.createElement("footer");</script> <![endif]-->
 </head>
 <body class="<?php echo $mode;?>">
@@ -290,8 +324,72 @@
 	
 	<form action="" method="get" >
 		<input type="hidden" name="lang" value="<?php echo LANGUAGE;?>"/>
-	<span class="logo"><?php echo LOGO1.LOGO2; ?></span><span><input type="text" name="q" placeholder="<?php echo msg('Search'); ?>" value="<?php  echo $q_txt; ?>"/><input type="submit" value="OK"/></span>
-	<?php if ($mode!=''){echo '<input type="hidden" name="mod" value="'.$mode.'"/>';}?>
+		<span class="logo"><?php echo LOGO1.LOGO2; ?></span>
+		<span>
+			<input type="text" name="q" placeholder="<?php echo msg('Search'); ?>" value="<?php  echo $q_txt; ?>"/>
+			<input type="submit" value="OK"/>
+		</span>
+		<?php 
+			if ($mode!=''){echo '<input type="hidden" name="mod" value="'.$mode.'"/>';}
+			if ($mode=='images'){
+				// ajout des options de recherche d'images
+				// couleur
+				$colors=array(
+					''=>msg('all colors'),
+					'ic:trans'=>'Transparent',
+					'ic:gray'=>msg('Black_and_white'),
+					'ic:color'=>msg('Color'),
+					'ic:specific,isc:red'=>'red',
+					'ic:specific,isc:orange'=>'orange',
+					'ic:specific,isc:yellow'=>'yellow',
+					'ic:specific,isc:pink'=>'pink',
+					'ic:specific,isc:white'=>'white',
+					'ic:specific,isc:gray'=>'gray',
+					'ic:specific,isc:black'=>'black',
+					'ic:specific,isc:brown'=>'brown',
+					'ic:specific,isc:green'=>'green',
+					'ic:specific,isc:teal'=>'teal',
+					'ic:specific,isc:blue'=>'blue',		
+				);
+				echo '<select id="color_selection" name="couleur" class="'.$colors[$couleur].'" title="'.msg('Select a color').'" onChange="change_class(this.options[this.selectedIndex].innerHTML);" >';
+				foreach ($colors as $get=>$color){
+					if ($get==$couleur){$sel=' selected ';}else{$sel='';}
+					echo '<option value="'.$get.'" class="'.$color.'"'.$sel.'>'.msg($color).'</option>';
+				}
+				echo '</select>';
+				unset($colors);
+				// tailles
+				$sizes=array(
+					''=>msg('all sizes'),
+					'isz:l'=>msg('Big'),
+					'isz:m'=>msg('Medium'),
+					'isz:i'=>msg('Icon'),
+					'isz:lt,islt:vga'=>'>  640x 480',
+					'isz:lt,islt:svga'=>'>  800x 600',
+					'isz:lt,islt:xga'=>'> 1024x 768',
+					'isz:lt,islt:2mp'=>'> 1600x1200 2mpx',
+					'isz:lt,islt:4mp'=>'> 2272x1704 4mpx',
+					'isz:lt,islt:6mp'=>'> 2816x2112 6mpx',
+					'isz:lt,islt:8mp'=>'> 3264x2448 8mpx',
+					'isz:lt,islt:10mp'=>'> 3648x2736 10mpx',
+					'isz:lt,islt:12mp'=>'> 4096x3072 12mpx',
+					'isz:lt,islt:15mp'=>'> 4480x3360 15mpx',
+					'isz:lt,islt:20mp'=>'> 5120x3840 20mpx',
+					'isz:lt,islt:40mp'=>'> 7216x5412 40mpx',
+					'isz:lt,islt:70mp'=>'> 9600x7200 70mpx',
+				);
+				echo '<select id="size_selection" name="taille" class="'.$sizes[$taille].'" title="'.msg('Select a size').'">';
+				foreach ($sizes as $get=>$size){
+					if ($get==$taille){$sel=' selected ';}else{$sel='';}
+					echo '<option value="'.$get.'"'.$sel.'>'.$size.'</option>';
+				}
+				echo '</select>';
+			}
+		?>
+	
+
+
+
 	</form>
 	<p class="msg">
 		<?php 
@@ -319,6 +417,12 @@
 	<a href="http://duckduckgo.com" title="<?php echo msg('Otherwise, use a real Search engine !');?>" class="ddg wot-exclude "> </a>
 </footer>
 <?php if(USE_WEB_OF_TRUST){echo '<script type="text/javascript" src="http://api.mywot.com/widgets/ratings.js"></script>';}?> 
+	<script language="javascript"> 
+		function change_class(classe) { 
+			var btn = document.getElementById("color_selection"); 
+			btn.className= classe; 
+		} 
+	</script>
 </body>
 </html>
 <?php add_search_engine(); ?>
